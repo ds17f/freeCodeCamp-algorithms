e96b5d33f1424b13c82d041f6dd1872a56fd2a2a
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2020-09-08 09:33:58 -0700

    add test cases for permutate

---
+++ src/03_StringPermutation/permutate.js
@@ -0,0 +1,8 @@
+
+const permAlone = () => {
+
+};
+
+module.exports = {
+  permAlone
+};
---
+++ src/03_StringPermutation/permutate.spec.js
@@ -0,0 +1,26 @@
+const {describe, test, expect} = require("@jest/globals");
+const {permAlone} = require("./permutate");
+
+describe('Test permutations', () => {
+
+  test('permAlone should return a number', () => {
+    expect(typeof permAlone()).toBe('number');
+  });
+
+
+  test.each([
+      ["aab", 2],
+      ["aaa", 0],
+      ["aabb", 8],
+      ["abcdefa", 3600],
+      ["abfdefa", 2640],
+      ["zzzzzzzz", 0],
+      ["a", 1],
+      ["aaab", 0],
+      ["aaabb", 12]
+    ]
+  )('fn(%j) should return %j', (inputs, expected) => {
+    expect(permAlone(inputs)).toBe(expected);
+  });
+
+});
