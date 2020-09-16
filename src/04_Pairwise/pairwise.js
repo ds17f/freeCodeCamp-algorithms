65936593bac3da6ebd75c0a51a6e30f8ad9d816c
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2020-09-16 06:49:00 -0700

    add Josh's insightful pairwise implementation

--- src/04_Pairwise/pairwise.js
+++ src/04_Pairwise/pairwise.js
@@ -27,6 +27,28 @@ const pairwise = (...args) => {
   return sum;
 };
 
+// An excellent solution in O(n) time proposed by my friend Josh Go
+// see more here: https://github.com/joshgo/freecodecamp-notes/blob/master/pairwise/pairwise.js
+function pairwise_JoshGo(arr, arg) {
+  const map = new Map();
+  let total = 0;
+
+  arr.forEach((x,i) => {
+    let y = arg - x; // x+y = arg, find y
+
+    if (map.has(y) && map.get(y).length > 0) {
+      let j = map.get(y).shift();
+      total += i + j;
+    } else if (map.has(x)) {
+      map.get(x).push(i);
+    } else {
+      map.set(x, [i]);
+    }
+  });
+
+  return total;
+}
 module.exports = {
-  pairwise
+  pairwise,
+  pairwise_JoshGo
 }
