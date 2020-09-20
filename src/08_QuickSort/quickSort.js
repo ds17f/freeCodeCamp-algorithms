f10b428d9487cc1d26a8b0805568a9c68e3ecc1b
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2020-09-20 08:58:48 -0700

    cleanup the quicksort stuff, implement both HOARE and LOMUTO partitions

--- src/08_QuickSort/quickSort.js
+++ src/08_QuickSort/quickSort.js
@@ -6,46 +6,22 @@ const swap = (A, x, y) => {
     [A[x], A[y]] = [A[y], A[x]]
 };
 
-
-const partition = (arr, lo, hi, side) => {
-  return parition_HOAR(arr, lo, hi)
-}
-
-const parition_HOAR = (arr, lo, hi) => {
-  const pivot = arr[Math.floor((hi + lo) / 2)];
-  let i = lo - 1;
-  let j = hi + 1;
-  while (true) {
-    do {
-      i++
-    } while (arr[i] < pivot)
-    do {
-      j--
-    } while (arr[j] > pivot)
-    if (i >= j) {
-      return j;
+const quickSortLomutoPartition = (arr, ...args) => {
+  const partition = () => {
+    const pValue = arr[hi];
+    let i = lo;
+    for (let j = lo; j < hi; j++) {
+      print({side, lo, hi, pValue, i, j, arr})
+      if ( arr[j] < pValue ) {
+        swap(arr, j, i)
+        i += 1;
+      }
     }
-    swap(arr, i, j)
-  }
-  return arr;
-}
+    swap(arr, i, hi)
+    print({i, [side]: JSON.stringify(arr)})
+    return i;
+  };
 
-const partition_orig = (arr, lo, hi, side) => {
-  const pValue = arr[hi];
-  let i = lo;
-  for (let j = lo; j < hi; j++) {
-    print({side, lo, hi, pValue, i, j, arr})
-    if ( arr[j] < pValue ) {
-      swap(arr, j, i)
-      i += 1;
-    }
-  }
-  swap(arr, i, hi)
-  print({i, [side]: JSON.stringify(arr)})
-  return i;
-};
-
-const quickSort = (arr, ...args) => {
   if (! args.length) {
     args = []
     args.push(0)
@@ -56,13 +32,49 @@ const quickSort = (arr, ...args) => {
 
   if ( lo < hi ) {
     const pivot = partition(arr, lo, hi, side);
-    quickSort(arr, lo, pivot, "lo")
-    quickSort(arr, pivot + 1, hi, "hi")
+    quickSortLomutoPartition(arr, lo, pivot - 1, "lo") //NOTE: this is the one diff between the quicksorts based on the partitions
+    quickSortLomutoPartition(arr, pivot + 1, hi, "hi")
+  }
+
+  return arr;
+}
+
+const quickSortHoarePartition = (arr, ...args) => {
+  const partition = (arr, lo, hi) => {
+    const pivot = arr[Math.floor((hi + lo) / 2)];
+    let i = lo - 1;
+    let j = hi + 1;
+    while (true) {
+      do {
+        i++
+      } while (arr[i] < pivot)
+      do {
+        j--
+      } while (arr[j] > pivot)
+      if (i >= j) {
+        return j;
+      }
+      swap(arr, i, j)
+    }
+  }
+
+  if (! args.length) {
+    args = []
+    args.push(0)
+    args.push(arr.length - 1)
+  }
+  const [lo, hi] = args;
+
+  if ( lo < hi ) {
+    const pivot = partition(arr, lo, hi);
+    quickSortHoarePartition(arr, lo, pivot)
+    quickSortHoarePartition(arr, pivot + 1, hi)
   }
 
   return arr;
 }
 
 module.exports = {
-  quickSort
+  quickSortHoarePartition,
+  quickSortLomutoPartition
 }
--- src/08_QuickSort/quickSort.spec.js
+++ src/08_QuickSort/quickSort.spec.js
@@ -1,9 +1,21 @@
 const {describe, test, expect} = require("@jest/globals");
-const { quickSort } = require("./quickSort");
+const { quickSortHoarePartition, quickSortLomutoPartition } = require("./quickSort");
 
 const fnName = "quickSort"
 
 describe(`Test ${fnName}`, () => {
+
+  test.each([
+      [[1,2,3]],
+      [[3,2,1]],
+      [[8,2,6,4,3,9]],
+      [[9,2,6,4,3,84]],
+      [[91, 23, 12, 41, 3]],
+      [[1,2,3,7,4,6,5,8,9,10,11]]
+    ]
+  )(`${fnName}Lomuto(%j) should sort correctly`, (inputs) => {
+    expect(quickSortLomutoPartition(inputs)).toStrictEqual([...inputs].sort((a,b) => a-b));
+  });
 
   test.each([
     [[1,2,3]],
@@ -13,8 +25,8 @@ describe(`Test ${fnName}`, () => {
     [[91, 23, 12, 41, 3]],
     [[1,2,3,7,4,6,5,8,9,10,11]]
     ]
-  )(`${fnName}(%j) should return %j`, (inputs) => {
-    expect(quickSort(inputs)).toStrictEqual([...inputs].sort((a,b) => a-b));
+  )(`${fnName}Hoare(%j) should sort correctly`, (inputs) => {
+    expect(quickSortHoarePartition(inputs)).toStrictEqual([...inputs].sort((a,b) => a-b));
   });
 
 });
