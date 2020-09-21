357e593e1696ab2ead7a61a765758fd7d2ce57a4
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2020-09-20 20:47:33 -0700

    wip

--- src/09_MergeSort/mergeSort.js
+++ src/09_MergeSort/mergeSort.js
@@ -2,8 +2,35 @@ const swap = (A, x, y) => {
   [A[x], A[y]] = [A[y], A[x]]
 };
 
-const mergeSort = arr => {
-  return [1,2,3]
+const mergeSort = (arr, ...args) => {
+	if (!args.length) {
+		args.push(0);
+		args.push(arr.length)
+	}
+	const[start, end] = args;
+	if ((end - start) < 2) {
+		// only 1 and that's sorted
+		return [args[start]];
+	} else {
+		const split = Math.floor((end + start) / 2);
+		const left = mergeSort(arr, start, split);
+		const right = mergeSort(arr, split + 1, end 
+		-1);
+		const tmp = [];
+		
+		let i = 0;
+		let j = 0;
+		while( i < left.length || j < right.length) {
+			if (left[i] < right[j]) {
+				tmp.push(left[i]);
+				i++;
+			} else {
+				tmp.push(right[j]);
+				j++;
+			}
+		}
+		return tmp;
+	}
 
 }
 
